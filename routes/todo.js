3fd479e76f19848bd0eb43b8f48501cd05679e62
lululthfh <punyalulu05@gmail.com>
2025-08-05 09:08:35 +0700

    feat: add endpoint

--- routes/todo.js
+++ routes/todo.js
@@ -5,3 +5,38 @@ let todos = [
   { id: 1, task: "Belajar Node.js", },
   { id: 2, task: "Membuat API", },
 ];
+
+router.get("/", (req, res) => {
+  res.json(todos);
+});
+
+router.get("/:id", (req, res) => {
+  const todo = todos.find((t) => t.id === parseInt(req.params.id));
+  if (!todo) return res.status(404).send("Tugas tidak ditemukan");
+  res.json(todo);
+});
+
+router.post("/", (req, res) => {
+  const newTodo = {
+    id: todos.length + 1,
+    task: req.body.task,
+  };
+  todos.push(newTodo);
+  res.status(201).json(newTodo);
+});
+
+router.put("/:id", (req, res) => {
+  const todo = todos.find((t) => t.id === parseInt(req.params.id));
+  if (!todo) return res.status(404).send("Tugas tidak ditemukan");
+
+  todo.task = req.body.task;
+  res.json(todo);
+});
+
+router.delete("/:id", (req, res) => {
+  const todoIndex = todos.findIndex((t) => t.id === parseInt(req.params.id));
+  if (todoIndex === -1) return res.status(404).send("Tugas tidak ditemukan");
+
+  todos.splice(todoIndex, 1);
+  res.status(204).send();
+});

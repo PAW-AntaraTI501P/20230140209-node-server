14186423bf86cb8021be4e787060ef6f46c78ae8
lululthfh <punyalulu05@gmail.com>
2025-08-05 09:36:28 +0700

    refactor: update port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = 3001;
 
 const todoRoutes = require("./routes/todo");
 const { todos } = require("./routes/todo");

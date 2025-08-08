ee850e06c1b7d7d1af4a2762974357b084cdee34
lululthfh <punyalulu05@gmail.com>
2025-08-08 10:17:04 +0700

    feat: add method put for todos-list

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const todoRoutes = require("./routes/todo.js");
@@ -11,39 +11,50 @@ app.use("/todos", todoRoutes);
 
 app.set("view engine", "ejs"); //utk ke halaman ejs
 
-app.get('/', (req, res) => {
-    res.render("index"); //render file ke index.ejs
-})
+app.get("/", (req, res) => {
+  res.render("index"); //render file ke index.ejs
+});
 
 app.get("/contact", (req, res) => {
-    res.render("contact"); //render ke file contact.ejs
-})
+  res.render("contact"); //render ke file contact.ejs
+});
 
-app.get("/todos-data", (req,res) =>{
-    res.json(todos);
-})
-app.get("/todos-list", (req,res) =>{
-    res.render("todos-page", {todos: todos});
-})
-app.post("todos-list/add", (res,req) => {
-    const newTaskText = req.body.task;
+app.get("/todos-data", (req, res) => {
+  res.json(todos);
+});
+app.get("/todos-list", (req, res) => {
+  res.render("todos-page", { todos: todos });
+});
+app.post("todos-list/add", (res, req) => {
+  const newTaskText = req.body.task;
 
-    if (newTaskText && newTaskText.trim() !==""){
-        todos.length > 0 ? Math.max(...todos.map((t)=> t.id))+1:1;
+  if (newTaskText && newTaskText.trim() !== "") {
+    todos.length > 0 ? Math.max(...todos.map((t) => t.id)) + 1 : 1;
 
-        const newTodo = {
-            id: newId,
-            task: newTaskText,
-        };
-        todos.push(newTodo);
-    }
-    res.redirect("/todos-list;");
-})
+    const newTodo = {
+      id: newId,
+      task: newTaskText,
+    };
+    todos.push(newTodo);
+  }
+  res.redirect("/todos-list;");
+});
+
+app.put("/todos-list/edit/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const updatedTaskText = req.body.task;
+
+  const todoIndex = todos.findIndex((t) => t.id === id);
+  if (todoIndex !== -1 && updatedTaskText && updatedTaskText.trim() !== "") {
+    todos[todoIndex].task = updatedTaskText.trim();
+  }
+  res.redirect("/todos-list");
+});
 
 app.use((req, res) => {
-    res.status(404).send("404 - Page not found");
+  res.status(404).send("404 - Page not found");
 });
 
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
